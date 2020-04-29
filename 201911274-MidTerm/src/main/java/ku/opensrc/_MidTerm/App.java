e1b4a7072a1aba564162d69c728c748614acd5d8
nonaninona <starcraft0529@gmail.com>
2020-04-29 10:18:48 +0900

    add try and catch, lastest version

--- 201911274-MidTerm/src/main/java/ku/opensrc/_MidTerm/App.java
+++ 201911274-MidTerm/src/main/java/ku/opensrc/_MidTerm/App.java
@@ -23,28 +23,34 @@ public class App
         JSONParser jsonParser = new JSONParser();
         
         //파일 경로는 다를 수 있음
-		FileReader reader = new FileReader("C:\\Users\\이현희\\Desktop\\02보관함\\오픈소스SW\\github\\201911274-MidTerm\\MidTerm.json");
-
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
-        JSONArray infoArray = (JSONArray) jsonObject.get("poem");
-        
-        for(int i=0;i<infoArray.size();i++) {
-        	JSONObject itemObject = (JSONObject) infoArray.get(i);
-        	String data = (String)itemObject.get("item");
+        try {
         	
-          	boolean isprint = false;
-        	for(int j=0;j<words.length;j++) {
-        		if(data.indexOf(words[j]) != -1)
-        			isprint = true;
-        	}
-        	if(!isprint) {
-        		continue;
-        	}
-        	
-        	System.out.println("item " + i + " : " + data);
+	    	FileReader reader = new FileReader("C:\\Users\\이현희\\Desktop\\02보관함\\오픈소스SW\\github\\201911274-MidTerm\\MidTerm.json");
+	  
+	        JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+	        JSONArray infoArray = (JSONArray) jsonObject.get("poem");
+	        
+	        for(int i=0;i<infoArray.size();i++) {
+	        	JSONObject itemObject = (JSONObject) infoArray.get(i);
+	        	String data = (String)itemObject.get("item");
+	        	
+	          	boolean isprint = false;
+	        	for(int j=0;j<words.length;j++) {
+	        		if(data.indexOf(words[j]) != -1)
+	        			isprint = true;
+	        	}
+	        	if(!isprint) {
+	        		continue;
+	        	}
+	        	
+	        	System.out.println("item " + i + " : " + data);
+	        }
+	        console.close();
+	        reader.close();
+	        
+        }catch(FileNotFoundException e) {
+        	System.out.println("get FileNotFoundException : " + e.getMessage() );
         }
-        console.close();
-        reader.close();
     	
     }
 }
